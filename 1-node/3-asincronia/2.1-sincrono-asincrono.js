63de2b79218d518b8961469ef7d07509530d3a43
Alvaro del Angel <nachodelangel96@gmail.com>
2022-08-17 21:48:51 -0500

    feat: agragamos codigo de asincronia

---
+++ 1-node/3-asincronia/2.1-sincrono-asincrono.js
@@ -0,0 +1,21 @@
+/**
+ * Código sincrono: Una linea se jecuta inmediatamente despues de la otra
+ * no hay codigo dependiente o bloqueante.
+ */
+// console.log('============== BLOQUE 1 ==============');
+//  console.log('Primero'); 
+//  console.log('Segundo');
+//  console.log('Tercero');
+
+ /**
+  * BLOQUE 2 - Problema tipico de asincronía
+  * 
+  */
+const validatingPayment = () => {
+    setTimeout(() => {
+        console.log('2 Validar la tarjeta de credito/debito y mostrar un loader');
+    }, 0 )
+}
+  console.log('1 Presionar el boton comprar');
+  validatingPayment();
+  console.log('3 Ver el mensaje de gracias por su compra joven');
